9651a385ae89a0f2aa5e74125845563f29dd791a
Koosy <koosyong@gmail.com>
2013-02-14 09:08:49 +0100

    d

---
+++ pointcloud/ckinect_cutting-build/CMakeFiles/ckinect_cutting.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/koosy/tum-lsr-dhri/pointcloud/include/ckinectfiltering.cpp" "/home/koosy/tum-lsr-dhri/pointcloud/ckinect_cutting-build/CMakeFiles/ckinect_cutting.dir/home/koosy/tum-lsr-dhri/pointcloud/include/ckinectfiltering.o"
+  "/home/koosy/tum-lsr-dhri/pointcloud/ckinect_cutting/src/ckinect_cutting.cpp" "/home/koosy/tum-lsr-dhri/pointcloud/ckinect_cutting-build/CMakeFiles/ckinect_cutting.dir/src/ckinect_cutting.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ pointcloud/ckinect_cutting-build/CMakeFiles/ckinect_cutting.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ckinect_cutting.dir/src/ckinect_cutting.o"
+  "CMakeFiles/ckinect_cutting.dir/home/koosy/tum-lsr-dhri/pointcloud/include/ckinectfiltering.o"
+  "/home/koosy/tum-lsr-dhri/pointcloud/ckinect_cutting/bin/ckinect_cutting.pdb"
+  "/home/koosy/tum-lsr-dhri/pointcloud/ckinect_cutting/bin/ckinect_cutting"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ckinect_cutting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
